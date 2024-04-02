1180134f1f00c62f97764d81282cac4fea90926a
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2024-04-02 14:51:07 -0500

    Minor updates to deletion scripts

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
-UUIDS="/storage1/fs1/dinglab/Active/Projects/scao/mild/datafreeze/MILD.UUID_to_delete.dat"
+UUIDS="/rdcw/fs2/home1/Active/home/m.wyczalkowski/Projects/GDAN/delete/09.HCMI_WGS_674/dat/HCMI.to_delete.dat"
+PROJECT="HCMI"
 
 # Existing files.  These are not modified
-CATALOG="/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/MILD.Catalog-REST.tsv"
-BAMMAP3="/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/WUSTL-BamMap/MILD.BamMap3.tsv"
-BAMMAPW="/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/WUSTL-BamMap/MILD.BamMap-wide.tsv"
+CATALOG="/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/${PROJECT}.Catalog-REST.tsv"
+BAMMAP3="/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/WUSTL-BamMap/${PROJECT}.BamMap3.tsv"
+BAMMAPW="/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/WUSTL-BamMap/${PROJECT}.BamMap-wide.tsv"
