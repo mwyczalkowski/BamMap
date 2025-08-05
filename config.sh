40407c871bd22e7712cedba247b6fc6ee575b21e
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2025-08-05 15:27:53 -0500

    Minor updates

--- config.sh
+++ config.sh
@@ -1,7 +1,23 @@
-UUIDS="/rdcw/fs2/home1/Active/home/m.wyczalkowski/Projects/GDAN/delete/09.HCMI_WGS_674/dat/HCMI.to_delete.dat"
-PROJECT="HCMI"
+UUIDS="dat/UUID_delete.dat"
+PROJECT="CPTAC4-STAD"
 
 # Existing files.  These are not modified
-CATALOG="/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/${PROJECT}.Catalog-REST.tsv"
+#CATALOG="/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/${PROJECT}.Catalog-REST.tsv"
+CATALOG="/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/${PROJECT}.Catalog3.tsv"
 BAMMAP3="/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/WUSTL-BamMap/${PROJECT}.BamMap3.tsv"
 BAMMAPW="/home/m.wyczalkowski/Projects/GDAN/GDAN.catalog/Catalog3/WUSTL-BamMap/${PROJECT}.BamMap-wide.tsv"
+
+if [ ! -e $CATALOG ]; then 
+    >&2 echo Error: Catalog does not exist: $CATALOG
+    exit 1
+fi
+
+if [ ! -e $BAMMAP3 ]; then 
+    >&2 echo Error: BAMMAP3 does not exist: $BAMMAP3
+    exit 1
+fi
+
+if [ ! -e $BAMMAPW ]; then 
+    >&2 echo Error: BAMMAPW does not exist: $BAMMAPW
+    exit 1
+fi
